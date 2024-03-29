b4989a145355f95b235e91f34a75abe4cf4d933a
mr-loop-1 <loopolecreator1@gmail.com>
2024-03-30 03:51:55 +0530

    add: logic of sql for divergin

---
+++ data/link.knex.ts
@@ -0,0 +1,27 @@
+import { knex } from "../database";
+import { ICustomer } from "../interfaces";
+
+export const updateLinkSecondary = async (
+    primaryId,
+    secondaryId
+): Promise<void> => {
+    const query = knex<ICustomer>("customers");
+
+    query.where("id", secondaryId);
+    query.update("linkedId", primaryId);
+
+    await query;
+
+    return Promise.resolve();
+};
+
+export const updateLinkMany = async (primaryId, linkId): Promise<void> => {
+    const query = knex<ICustomer>("customers");
+
+    query.where("linkedId", linkId);
+    query.update("linkedId", primaryId);
+
+    await query;
+
+    return Promise.resolve();
+};
